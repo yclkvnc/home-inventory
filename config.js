842b35e07e27dbd27953ab3b98e5a434250a3f03
Yücel Kıvanç <yucel.kivanc@gmail.com>
2026-08-01 23:44:46 +0200

    Update config
    Updated configuration values for Azure App Registration and OneDrive.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 
 const CONFIG = {
   // Azure App Registration → Overview → Application (client) ID
-  clientId: "YOUR_AZURE_APP_CLIENT_ID",
+  clientId: "6557da9d-9eaa-4b9d-b9f2-7820570f2abc",
 
   // Personal Microsoft accounts only
   authority: "https://login.microsoftonline.com/consumers",
@@ -17,10 +17,10 @@ const CONFIG = {
   redirectUri: "https://yclkvnc.github.io/home-inventory/",
 
   // OneDrive item ID of /HomeInventory/inventory.xlsx
-  excelFileId: "YOUR_EXCEL_FILE_ONEDRIVE_ITEM_ID",
+  excelFileId: "DC4CDEC5003A3E1B!s252c95f238434126a58b3487974183e8",
 
   // OneDrive item ID of the /HomeInventory/ folder
-  folderId: "YOUR_HOMEINVENTORY_FOLDER_ONEDRIVE_ITEM_ID",
+  folderId: "DC4CDEC5003A3E1B!sca675cafb9994226b6080d938144b6f5",
 
   // Name of the Excel table inside inventory.xlsx
   tableName: "Table1",
